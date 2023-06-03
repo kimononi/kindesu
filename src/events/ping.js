ecd963fe92fc5ccada1bd08b6be3452da38b2593
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 03:44:08 +0700

    handle ping interaction

---
+++ src/events/ping.js
@@ -0,0 +1,10 @@
+import { discord } from './deps.js';
+
+export default {
+  type: discord.InteractionType.Ping,
+  async execute(ctx) {
+    ctx.response.body = {
+      type: discord.InteractionResponseType.Pong,
+    };
+  }
+};
