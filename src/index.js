ce666aedb338bfaab59291a8b0953f0a24f3aaf5
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-03 20:44:21 +0700

    application setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { oak } from './deps.js';
+
+const router = new oak.Router();
+
+const app = new oak.Application();
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen({ port: 80 });
