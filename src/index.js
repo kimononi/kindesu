ee9383e714788bc59a637b3189a0afd97c5b28d6
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 04:55:17 +0700

    set application proxy to true

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const routes = Array.from(Deno.readDirSync(Deno.cwd() + '/src/events'))
 Promise.all(routes.map(ctx => import(`./events/${ctx.name}`)))
   .then(console.log);
 
-const app = new oak.Application();
+const app = new oak.Application({ proxy: true });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
