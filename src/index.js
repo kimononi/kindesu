c0d32d42aaff205b5feddd48512a67d4ee3c12b8
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 06:29:55 +0700

    fix routes handler

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { oak } from './deps.js';
-import * as routes from './routes/mod.ts';
+import * as routes from './routes/mod.js';
 
 const router = new oak.Router();
+
+for await (const route of Object.values(routes)) {
+  router.add(route.method, route.path, route.execute);
+}
 
 const app = new oak.Application({ proxy: true });
 
