d28d3abde0e629b8dfaa498126aba03c160fe3a1
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 03:21:36 +0700

    export discord_api_types@0.37.43

--- src/deps.js
+++ src/deps.js
@@ -1,2 +1,3 @@
+export * as discord from 'https://deno.land/x/discord_api_types@0.37.43/v10.ts';
 export * as nacl from 'https://esm.sh/tweetnacl@1.0.3';
 export * as oak from 'https://deno.land/x/oak@v12.5.0/mod.ts';
