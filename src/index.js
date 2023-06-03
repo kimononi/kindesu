8f2c280e2f112995e8860e4c98b2db6d3f278612
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 03:18:13 +0700

    handle routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import { oak } from './deps.js';
 
 const router = new oak.Router();
 
+for await (const file of Deno.readDir(Deno.cwd() + '/src/routes')) {
+  const { default: route } = await import(`./routes/${file.name}`);
+  router.add(route.method, route.path, route.execute);
+}
+
 const app = new oak.Application();
 
 app.use(router.routes());
