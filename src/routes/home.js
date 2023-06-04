3eb7076b6ccf16302f5e38a1c6775a07d4572761
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 12:19:01 +0700

    fix cannot access variable before initialization

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,8 +6,8 @@ export default {
   async execute(ctx) {
     const isAllowed = await ctx.cookies.get('allowed');
     if (isAllowed) {
-      const routes = Object.values(routes).filter(route => route.default.method === 'GET' && route.default.path !== Deno.env.get('SECRET'));
-      ctx.response.body = `Welcome bek bang admin^^!\n\n${routes.map(route.default.path).join(" ")}`
+      const routesPaths = Object.values(routes).filter(route => route.default.method === 'GET' && route.default.path !== Deno.env.get('SECRET'));
+      ctx.response.body = `Welcome bek bang admin^^!\n\n${routesPaths.map(route.default.path).join(" ")}`
     } else {
       ctx.response.body = `Kamu admin? kalau iya, silahkan pergi ke ${ctx.request.url.origin}/<secret> untuk akses website ini ^^\n* secret bisa diambil di panel deno deploy`
     }
