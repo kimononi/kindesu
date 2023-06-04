df2cd996772dd5b3ad910fc49f14181d37d7f003
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 12:28:06 +0700

    fix route filtering

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,7 +6,7 @@ export default {
   async execute(ctx) {
     const isAllowed = await ctx.cookies.get('allowed');
     if (isAllowed) {
-      const routesPaths = Object.values(routes).filter(route => route.default.method === 'GET' && ['/', `/${Deno.env.get('SECRET')}`].includes(route.default.path));
+      const routesPaths = Object.values(routes).filter(route => (route.default.method === 'GET') && !['/', `/${Deno.env.get('SECRET')}`].includes(route.default.path));
       ctx.response.body = `Welcome bek bang admin^^!\n\n${routesPaths.map(route => route.default.path).join(" ")}`
     } else {
       console.log(`[secret request: ${ctx.request.ip}]: ${Deno.env.get('SECRET')}`)
