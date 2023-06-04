aac5a933b1067a67dc44d25ba5f936d7ff4550b8
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 08:13:20 +0700

    update ping interaction

--- src/events/ping.js
+++ src/events/ping.js
@@ -2,7 +2,7 @@ import { discord } from './deps.js';
 
 export default {
   type: discord.InteractionType.Ping,
-  async execute(ctx) {
+  async execute({ ctx }) {
     ctx.response.body = {
       type: discord.InteractionResponseType.Pong,
     };
