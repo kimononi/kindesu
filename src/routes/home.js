17552916252337417871857cf31f284e2a533ade
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 12:20:39 +0700

    fix undefined 'route'

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,7 +7,7 @@ export default {
     const isAllowed = await ctx.cookies.get('allowed');
     if (isAllowed) {
       const routesPaths = Object.values(routes).filter(route => route.default.method === 'GET' && route.default.path !== Deno.env.get('SECRET'));
-      ctx.response.body = `Welcome bek bang admin^^!\n\n${routesPaths.map(route.default.path).join(" ")}`
+      ctx.response.body = `Welcome bek bang admin^^!\n\n${routesPaths.map(route => route.default.path).join(" ")}`
     } else {
       ctx.response.body = `Kamu admin? kalau iya, silahkan pergi ke ${ctx.request.url.origin}/<secret> untuk akses website ini ^^\n* secret bisa diambil di panel deno deploy`
     }
