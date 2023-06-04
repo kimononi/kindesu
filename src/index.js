aab655a42405129d219cbab80177def3ee133d7c
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 07:51:39 +0700

    undo all changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import { oak } from './deps.js';
-import * as routes from './routes/mod.js';
 
 const router = new oak.Router();
 
-for await (const route of Object.values(routes)) {
-  router.add(route.method, route.path, route.execute);
-}
+// 
 
 const app = new oak.Application({ proxy: true });
 
