8ce83742c76e9760a16febc92bfdda35b0074226
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 08:32:40 +0700

    add routes handler

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import * as routes from './routes/mod.js';
 
 const router = new oak.Router();
 
-// 
+for (const route of Object.values(routes).map(ctx => ctx.default)) {
+  router.add(route.method, route.path, route.execute);
+};
 
 const app = new oak.Application({ proxy: true });
 
