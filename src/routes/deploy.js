b7ed848608ddb4a3e3ae72d093e44e20f311fed6
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 17:32:15 +0700

    remove some script to debug error

--- src/routes/deploy.js
+++ src/routes/deploy.js
@@ -6,20 +6,6 @@ export default {
   path: '/deploy',
   async execute(ctx) {
     const isAllowed = await ctx.cookies.has('allowed');
-    if (!isAllowed) {
-      return ctx.response.redirect('/');
-    } else {
-      const branch = ctx.request.url.host.includes('--') ? ctx.request.url.host.split('--')[1].split('.deno.dev')[0].toUpperCase() : 'PROD';
-      const body = Object.values(commands).map(cmd => cmd.default.data);
-      
-      const deployRequest = await fetch(discord.RouteBases.api + discord.Routes.applicationCommands(Deno.env.get(`${branch}_ID`)), {
-        method: 'PUT',
-        headers: { Authorization: `Bot ${Deno.env.get(`${branch}_TOKEN`)}` },
-        body
-      });
-      const deployResult = await deployRequest.body();
-      
-      ctx.response.body = JSON.stringify(deployResult, null, ' ');
-    }
+    
   }
 }
