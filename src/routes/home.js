7fc2f07b282ee09b37575aff29d78d4f19a447de
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 12:08:39 +0700

    home page setup

---
+++ src/routes/home.js
@@ -0,0 +1,15 @@
+import * as routes from './mod.js';
+
+export default {
+  method: 'GET',
+  path: '/',
+  async execute(ctx) {
+    const isAllowed = await ctx.cookies.get('allowed');
+    if (isAllowed) {
+      const routes = Object.values(routes).filter(route => route.default.method === 'GET' && route.default.path !== Deno.env.get('SECRET'));
+      ctx.response.body = `Welcome bek bang admin^^!\n\n${routes.map(route.default.path).join(" ")}`
+    } else {
+      ctx.response.body = `Kamu admin? kalau iya, silahkan pergi ke ${ctx.request.url.origin}/<secret> untuk akses website ini ^^\n* secret bisa diambil di panel deno deploy`
+    }
+  }
+}
