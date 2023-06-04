771b41aebdc0ddc1b707187fe212cf1e503575d0
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 12:16:13 +0700

    fix login route path

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,6 +1,6 @@
 export default {
   method: "GET",
-  path: Deno.env.get('SECRET'),
+  path: `/${Deno.env.get('SECRET')}`,
   async execute(ctx) {
     await ctx.cookies.set('allowed', 'true');
     ctx.response.redirect('/');
