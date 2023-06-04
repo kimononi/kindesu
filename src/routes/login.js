b76c82d6994c90941adca1ceccb4b5a84f010af1
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-04 12:13:41 +0700

    update cookies set value

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,7 +2,7 @@ export default {
   method: "GET",
   path: Deno.env.get('SECRET'),
   async execute(ctx) {
-    await ctx.cookies.set('allowed', true);
+    await ctx.cookies.set('allowed', 'true');
     ctx.response.redirect('/');
   }
 }
