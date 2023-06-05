ef4c94e20dcff7a437314700cba2f14aab87e344
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-05 19:24:12 +0700

    setup say command autocomplete

--- src/commands/say.js
+++ src/commands/say.js
@@ -6,9 +6,23 @@ export default {
     description: '🍥゛Kirim pesan sebagai bot.',
     type: discord.ApplicationCommandType.ChatInput,
     dm_permission: false,
-    default_member_permissions: (discord.PermissionFlagsBits.Administrator).toString()
+    default_member_permissions: (discord.PermissionFlagsBits.Administrator).toString(),
+    options: [{
+      name: 'content',
+      description: '🍥゛Kamu teh mau bilang apa?',
+      type: discord.ApplicationCommandOptionType.String,
+      required: true,
+      autocomplete: true
+    }]
   },
   async execute({ ctx, interaction, branch }) {
-    
+    if (interaction.type === discord.InteractionType.ApplicationCommandAutocomplete) {
+      ctx.response.body = {
+        type: discord.InteractionResponseType.ApplicationCommandAutocompleteResult,
+        data: { choices: [] }
+      };
+    } else {
+      
+    }
   }
 }
