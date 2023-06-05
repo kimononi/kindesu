eecf29e83c47dde4b846c255759f21c1990a1627
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-05 15:54:04 +0700

    added test script back

--- src/routes/test.js
+++ src/routes/test.js
@@ -1,10 +1,28 @@
 import { discord } from '../deps.js';
-import rawConfig from '../config.js';
+import * as rawConfig from '../config.js';
 
 export default {
   method: 'GET',
   path: '/test',
   async execute(ctx) {
+    const isAllowed = await ctx.cookies.has('allowed');
+    if (!isAllowed) return ctx.response.redirect('/');
     
+    const branch = ctx.request.url.host.includes('--') ? ctx.request.url.host.split('--')[1].split('.deno.dev')[0].toUpperCase() : 'PROD';
+    const config = rawConfig[branch];
+
+    const testRequest = await fetch(discord.RouteBases.api + discord.Routes.channelMessages(config.verificationChannel), {
+      method: 'POST',
+      headers: {
+        Authorization: `Bot ${Deno.env.get(`${branch}_TOKEN`)}`,
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        content: `## Verifikasi!\n>>> Sebelum verifikasi, pastikan kamu udah baca\n<#969506184199540796> disini dan siap menerima hukuman apabila kamu melanggar peraturan`
+      })
+    });
+    const testResult = await testRequest.json();
+
+    ctx.response.body = JSON.stringify(testResult, null, '  ');
   }
 }
