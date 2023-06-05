2baeaa03f871dcefebf178eccbe89b62142934e9
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-05 15:49:52 +0700

    try different export method

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
-export const PROD = {
+const PROD = {
   verificationChannel: '1115064050875891742'
 };
 
-export const CANARY = {
+const CANARY = {
   verificationChannel: '1115194663872253992'
 };
+
+export { PROD, CANARY };
