044f3cc279f82776c6ef8309b2cc0e7500986497
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-05 19:39:20 +0700

    export autocomplete function

--- src/events/mod.js
+++ src/events/mod.js
@@ -1,3 +1,4 @@
 export * as applicationCommand from './applicationCommand.js';
+export * as autocomplete from './autocomplete.js';
 export * as modalSubmit from './modalSubmit.js';
 export * as ping from './ping.js';
