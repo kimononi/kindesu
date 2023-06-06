1d6c33a7587b01f5b6c77715fc456fe09f9779b4
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-06 09:18:18 +0700

    remove dynamic import

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,13 +6,6 @@ export default {
   async execute(ctx) {
     const isAllowed = await ctx.cookies.get('allowed');
     
-    const dir = Array.from(Deno.readDirSync(Deno.cwd() + '/src/routes'))
-      .filter(file => file.name.endsWith('.js'));
-    console.log(dir);
-    
-    Promise.all(dir.map(file => import(`./` + file.name)))
-      .then(console.log);
-    
     if (isAllowed) {
       const routesPaths = Object.values(routes).filter(route => (route.default.method === 'GET') && !['/', `/${Deno.env.get('SECRET')}`].includes(route.default.path));
       ctx.response.body = `Welcome bek bang admin^^!\n\n${routesPaths.map(route => route.default.path).join(" ")}`
