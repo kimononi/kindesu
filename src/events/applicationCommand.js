35d119e7585c1de10671cbce35475a9724998db8
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 16:04:14 +0700

    fix cannot read properties of undefined

--- src/events/applicationCommand.js
+++ src/events/applicationCommand.js
@@ -4,6 +4,6 @@ import * as commands from '../commands/mod.js';
 export const type = InteractionType.ApplicationCommand;
   
 export async function execute({ ctx, interaction, branch }) {
-  const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type);
+  const command = Object.values(commands).find(cmd => cmd.data.name === interaction.data.name && cmd.data.type === interaction.data.type);
   if (command) return await command.default.execute({ ctx, interaction, branch });
 };
