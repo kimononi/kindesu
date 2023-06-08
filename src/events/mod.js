79ef3ad5344051df371eb4bf8c13b450648b1267
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 14:48:40 +0700

    .

--- src/events/mod.js
+++ src/events/mod.js
@@ -1,4 +1,3 @@
 export * as applicationCommand from './applicationCommand.js';
-export * as autocomplete from './autocomplete.js';
 export * as modalSubmit from './modalSubmit.js';
 export * as ping from './ping.js';
