239c8b39a56bda22098c06cbc5e6b0311bbe5e3f
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 22:12:48 +0700

    verification interaction setup

--- src/components/verification.js
+++ src/components/verification.js
@@ -1,15 +1,33 @@
-import { InteractionResponseType } from "discord_api_types/v10.ts";
+import { 
+  InteractionResponseType,
+  MessageFlags
+} from "discord_api_types/v10.ts";
 
 export const custom_id = "verification";
 
-export async function execute({ ctx, branch, interaction }) {
+export async function execute({ ctx }) {
   const quiz = await fetch("https://iciepex-nori-quiz.deno.dev/")
     .then(res => res.formData());
   
   const asset = quiz.get('asset');
   const payload = quiz.get('payload');
   
-  const payloadMessage = {
-    content: `## ${payload.question}\n${payload.answers.map(({ text }) => `* ${text}`).join("\n")}`
+  const payloadJSON = {
+    type: InteractionResponseType.ChannelMessageWithSource,
+    data: {
+      content: `## ${payload.question}\n${payload.answers.map(({ text }) => `* ${text}`).join("\n")}`,
+      attachments: [{
+        id: "0",
+        filename: "image.jpeg"
+      }],
+      flags: MessageFlags.Ephemeral
+    }
   };
+  
+  const formData = new FormData();
+  
+  formData.set("payload_json", JSON.stringify(payloadJSON));
+  formData.set("files[0]", asset, "image.jpeg");
+  
+  ctx.response.body = formData;
 };
