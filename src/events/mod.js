8fb13b985ff942b867b6d8543f9383528ca23df1
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 22:17:55 +0700

    export message component handler

--- src/events/mod.js
+++ src/events/mod.js
@@ -1,3 +1,4 @@
 export * as applicationCommand from './applicationCommand.js';
+export * as messageComponents from './messageComponent.js';
 export * as modalSubmit from './modalSubmit.js';
 export * as ping from './ping.js';
