650d9e468211b00ec8391dd4ee3a49716d0957cc
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 14:49:59 +0700

    syntax fix

--- src/events/applicationCommand.js
+++ src/events/applicationCommand.js
@@ -1,9 +1,9 @@
 import { InteractionType } from 'discord_api_types/v10.ts';
 import * as commands from '../commands/mod.js';
 
-export const type = InteractionType.ApplicationCommand,
+export const type = InteractionType.ApplicationCommand;
   
 export async function execute({ ctx, interaction, branch }) {
-    const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type);
-    if (command) return await command.default.execute({ ctx, interaction, branch });
+  const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type);
+  if (command) return await command.default.execute({ ctx, interaction, branch });
 };
