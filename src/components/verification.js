395025cb6b94194bc6249217f0acc90bfd1bffa6
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 23:42:23 +0700

    remove form data

--- src/components/verification.js
+++ src/components/verification.js
@@ -19,19 +19,20 @@ export async function execute({ ctx }) {
   console.log(payload);
   console.log(asset);
   
-  const payloadJSON = {
+  // const payloadJSON = {
+  ctx.response.body = {
     type: InteractionResponseType.ChannelMessageWithSource,
     data: {
-      content: `## ${payload.question}\n${payload.answers.map(({ text }) => `* ${text}`).join("\n")}`,
+      content: `## ${payload.question}\n${payload.answers.map(({ text }) => `* ${text}`).join("\n")}\n [](${baseURL + `/asset${payload.id}`})`,
       flags: MessageFlags.Ephemeral
     }
   };
   
-  const formData = new FormData();
+  // const formData = new FormData();
   
-  formData.set("payload_json", JSON.stringify(payloadJSON));
+  // formData.set("payload_json", JSON.stringify(payloadJSON));
   // formData.set("files[0]", asset, "image.jpeg");
   
-  ctx.response.body = formData;
+  // ctx.response.body = formData;
   // ctx.response.type = "form-data";
 };
