943e6bf62ce1786e4456b19102642d411fbcbe48
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 16:05:54 +0700

    same error :P

--- src/events/applicationCommand.js
+++ src/events/applicationCommand.js
@@ -5,5 +5,5 @@ export const type = InteractionType.ApplicationCommand;
   
 export async function execute({ ctx, interaction, branch }) {
   const command = Object.values(commands).find(cmd => cmd.data.name === interaction.data.name && cmd.data.type === interaction.data.type);
-  if (command) return await command.default.execute({ ctx, interaction, branch });
+  if (command) return await command.execute({ ctx, interaction, branch });
 };
