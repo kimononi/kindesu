ad1e951ca1f34923af6e0d2a0a77dbed80f05abb
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 14:46:54 +0700

    .

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -1,8 +1,8 @@
-import { discord } from '../deps.js';
+import { InteractionType } from 'discord_api_types/v10.ts';
 import * as modals from '../modals/mod.js';
 
 export default {
-  type: discord.InteractionType.ModalSubmit,
+  type: InteractionType.ModalSubmit,
   async execute({ ctx, interaction, branch }) {
     const modal = Object.values(modals).find(modal => modal.default.custom_id === interaction.data.custom_id);
     if (modal) return await modal.default.execute({ ctx, interaction, branch });
