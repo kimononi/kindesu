b718e8daaae00bb9965b93b8249663f072b80d62
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 14:49:37 +0700

    .

--- src/events/applicationCommand.js
+++ src/events/applicationCommand.js
@@ -1,10 +1,9 @@
 import { InteractionType } from 'discord_api_types/v10.ts';
 import * as commands from '../commands/mod.js';
 
-export default {
-  type: InteractionType.ApplicationCommand,
-  async execute({ ctx, interaction, branch }) {
+export const type = InteractionType.ApplicationCommand,
+  
+export async function execute({ ctx, interaction, branch }) {
     const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type);
     if (command) return await command.default.execute({ ctx, interaction, branch });
-  }
-}
+};
