32bcdeb87028f9bb7915e770a0fa43904c64bd84
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 14:53:11 +0700

    .

--- src/events/ping.js
+++ src/events/ping.js
@@ -3,11 +3,10 @@ import {
   InteractionResponseType
 } from 'discord_api_types/v10.ts';
 
-export default {
-  type: InteractionType.Ping,
-  async execute({ ctx }) {
-    ctx.response.body = {
-      type: InteractionResponseType.Pong,
-    };
-  }
+export const type = InteractionType.Ping;
+
+export async function execute({ ctx }) {
+  ctx.response.body = {
+    type: InteractionResponseType.Pong,
+  };
 };
