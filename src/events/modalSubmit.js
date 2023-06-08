b419c8363d28604bcafafcaaec54dafcf01b3596
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 16:08:02 +0700

    still the same error :P

--- src/events/modalSubmit.js
+++ src/events/modalSubmit.js
@@ -4,6 +4,6 @@ import * as modals from '../modals/mod.js';
 export const type = InteractionType.ModalSubmit;
 
 export async function execute({ ctx, interaction, branch }) {
-  const modal = Object.values(modals).find(modal => modal.default.custom_id === interaction.data.custom_id);
-  if (modal) return await modal.default.execute({ ctx, interaction, branch });
+  const modal = Object.values(modals).find(modal => modal.custom_id === interaction.data.custom_id);
+  if (modal) return await modal.execute({ ctx, interaction, branch });
 };
