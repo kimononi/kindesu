4c0f48fcc5b2b8dd905d7a31e02b0a51e71cd93e
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 15:47:09 +0700

    remove body response comment

--- src/routes/home.js
+++ src/routes/home.js
@@ -7,8 +7,8 @@ export async function execute(ctx) {
   const isAllowed = await ctx.cookies.get('allowed');
     
   if (isAllowed) {
-    // const routesPaths = Object.entries(routes).filter([route, data] => (data.method === 'GET') && !['/', `/${Deno.env.get('SECRET')}`].includes(data.path));
-    // ctx.response.body = JSON.stringify(Object.fromEntries(routesPaths.map([route, data] => [route, data.path])), null, '  ');
+    const routesPaths = Object.entries(routes).filter([route, data] => (data.method === 'GET') && !['/', `/${Deno.env.get('SECRET')}`].includes(data.path));
+    ctx.response.body = JSON.stringify(Object.fromEntries(routesPaths.map([route, data] => [route, data.path])), null, '  ');
   } else {
     console.log(`[secret request: ${ctx.request.ip}]: ${Deno.env.get('SECRET')}`)
     ctx.response.body = { message: `Kamu tidak terautentikasi nih, masukkan kode secret pada url ini (${ctx.request.url.origin + "/<secret>"})` };
