63663e03c756512ebb5c1e2311e60cae0a5aaef2
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-06-08 22:03:40 +0700

    verifikasi payload message setup

--- src/components/verification.js
+++ src/components/verification.js
@@ -3,5 +3,13 @@ import { InteractionResponseType } from "discord_api_types/v10.ts";
 export const custom_id = "verification";
 
 export async function execute({ ctx, branch, interaction }) {
-  const formData = await fetch("https://iciepex-nori-quiz.deno.dev/")
+  const quiz = await fetch("https://iciepex-nori-quiz.deno.dev/")
+    .then(res => res.formData());
+  
+  const asset = quiz.get('asset');
+  const payload = quiz.get('payload');
+  
+  const payloadMessage = {
+    content: `## ${payload.question}\n${payload.answers.map(({ text }) => `* ${text}`).join("\n")}`
+  };
 };
